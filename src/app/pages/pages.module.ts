c71d0b645f68cafdc757553532251020e86edc48
Fernando Rosales <rosalesvegafernando@gmail.com>
2021-07-07 20:53:52 -0600

    fin seccion 4

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NopagefoundComponent } from '../nopagefound/nopagefound.component';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    Grafica1Component,
+    ProgressComponent,
+    DashboardComponent,
+    NopagefoundComponent,
+    PagesComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class PagesModule { }
